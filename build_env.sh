3101607d58c5e347523322f453667c6c8bfb0b65
Bing <qxb.510@gmail.com>
2026-05-11 17:03:53 +0800

    集成droidspaces

--- build_env.sh
+++ build_env.sh
@@ -1,11 +1,17 @@
 # 内核编译环境变量
 export ARCH=arm64
-export CC=clang
+#export CC=clang
+#export CROSS_COMPILE=aarch64-linux-gnu-
 export CLANG_TRIPLE=aarch64-linux-gnu-
+
+export CC=aarch64-linux-gnu-gcc-11
 export CROSS_COMPILE=aarch64-linux-gnu-
 
 # 可选：设置编译产物输出目录（根据你的习惯选择是否启用）
 export O=out
 
-# 可选：使用所有CPU核心加速编译
-export MAKEFLAGS="-j12"
+# source build_env.sh
+# make clean && make mrproper
+# make blu_spark_defconfig
+# make -j12 2>&1 | tee build.log
+# make menuconfig
